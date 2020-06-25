94197170d547e818747a961f950acdc70296f4a5
finngreiter <finn.greiter@gmail.com>
2020-06-25 07:06:34 -0700

    fixed tak executable

--- takinstall.sh
+++ takinstall.sh
@@ -3,8 +3,9 @@ mkdir -p $TAK_LOCATION
 mkdir -p $TAK_LOCATION/bin
 mkdir -p $TAK_LOCATION/files
 curl -fsSL https://raw.githubusercontent.com/finngreiter/tak/master/packages/tak -o $TAK_LOCATION/bin/tak
+chmod +x $TAK_LOCATION/bin/tak
 clear
 echo '### TAK ###'
 echo 'install complete!'
 echo '### TAK ###'
-echo add $TAK_LOCATION/bin to path
+echo please add $TAK_LOCATION/bin to path
